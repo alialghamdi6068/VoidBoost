f4c8daa10f6f7feadad354e8ba087785c05c949e
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-17 16:54:55 +0300

    Add VoidBoost developer signature

--- src/main/java/com/voidboost/client/VoidBoostScreen.java
+++ src/main/java/com/voidboost/client/VoidBoostScreen.java
@@ -120,6 +120,7 @@ public final class VoidBoostScreen extends Screen {
                 VoidBoostConfig.get().ultimateLocked
                         ? "Ultimate performance is locked"
                         : "Client-side FPS & PvP optimization"), this.width / 2, 42, 0xAAAAAA);
+        graphics.drawCenteredString(this.font, Component.literal("Made by alialghamdi6068"), this.width / 2, 51, 0xAAAAAA);
         super.render(graphics, mouseX, mouseY, delta);
     }
 }
