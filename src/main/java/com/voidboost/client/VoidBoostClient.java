7bc08c14f835aca9ad836ac64736165f4f398691
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-17 17:36:20 +0300

    Open VoidBoost UI with a dedicated keybind

--- src/main/java/com/voidboost/client/VoidBoostClient.java
+++ src/main/java/com/voidboost/client/VoidBoostClient.java
@@ -2,13 +2,31 @@ package com.voidboost.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import org.lwjgl.glfw.GLFW;
 
 public final class VoidBoostClient implements ClientModInitializer {
     public static final String MOD_ID = "voidboost";
+    private static KeyMapping openMenuKey;
 
     @Override
     public void onInitializeClient() {
         VoidBoostConfig.load();
-        ClientTickEvents.END_CLIENT_TICK.register(VoidBoostConfig::tick);
+
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.voidboost.open_menu",
+                GLFW.GLFW_KEY_O,
+                "category.voidboost"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            VoidBoostConfig.tick(client);
+            while (openMenuKey.consumeClick()) {
+                if (client.screen == null) {
+                    client.setScreen(new VoidBoostScreen(null));
+                }
+            }
+        });
     }
 }
