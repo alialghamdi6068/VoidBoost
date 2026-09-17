dda6dd0038e51658d868ff972b643951f77b9894
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-17 22:06:15 +0300

    Fix title-screen key handler implementation

--- src/main/java/com/voidboost/mixin/TitleScreenMixin.java
+++ src/main/java/com/voidboost/mixin/TitleScreenMixin.java
@@ -1,8 +1,10 @@
 package com.voidboost.mixin;
 
 import com.voidboost.client.VoidBoostScreen;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.network.chat.Component;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,14 +13,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin extends Screen {
-    protected TitleScreenMixin(net.minecraft.network.chat.Component title) {
+    protected TitleScreenMixin(Component title) {
         super(title);
     }
 
     @Inject(method="keyPressed", at=@At("HEAD"), cancellable=true)
     private void voidboost$openMenu(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
         if(keyCode==GLFW.GLFW_KEY_O) {
-            ((Screen)(Object)this).getMinecraft().setScreen(new VoidBoostScreen((Screen)(Object)this));
+            Minecraft.getInstance().setScreen(new VoidBoostScreen((Screen)(Object)this));
             cir.setReturnValue(true);
         }
     }
