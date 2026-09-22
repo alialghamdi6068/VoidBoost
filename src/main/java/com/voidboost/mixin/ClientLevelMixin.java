f1411da38b356115dff4f64fb36d61fe56701328
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-22 22:14:49 +0300

    Make particle sampler field uniquely owned

--- src/main/java/com/voidboost/mixin/ClientLevelMixin.java
+++ src/main/java/com/voidboost/mixin/ClientLevelMixin.java
@@ -5,12 +5,14 @@ import com.voidboost.client.VoidBoostStats;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.particles.ParticleOptions;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientLevel.class)
 public abstract class ClientLevelMixin {
+    @Unique
     private static int voidboost$particleCounter;
 
     /**
