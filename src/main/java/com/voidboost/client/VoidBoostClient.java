7a0e56cab715d0ed224062eae553049bef1159ed
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-23 18:39:02 +0300

    Optimize client workload and keep Sodium compatibility

--- src/main/java/com/voidboost/client/VoidBoostClient.java
+++ src/main/java/com/voidboost/client/VoidBoostClient.java
@@ -1,10 +1,8 @@
 package com.voidboost.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.client.Minecraft;
 import net.minecraft.resources.Identifier;
 import org.lwjgl.glfw.GLFW;
 
@@ -20,7 +18,6 @@ public final class VoidBoostClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         VoidBoostConfig.load();
-        VoidBoostAI.initialize(Minecraft.getInstance());
 
         toggleMonitorKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 "key.voidboost.toggle_monitor",
@@ -28,9 +25,7 @@ public final class VoidBoostClient implements ClientModInitializer {
                 VOIDBOOST_CATEGORY
         ));
 
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            VoidBoostAI.tick(client);
-
+        net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (toggleMonitorKey.consumeClick()) {
                 VoidBoostConfig.togglePerformanceMonitor();
             }
